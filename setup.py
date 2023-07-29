51724edf05e320df51258a8f4d343fd0f28af718
Omer Majdi <omermajdi@Omers-MacBook-Air.local>
2023-07-29 20:19:13 +0100

    final package

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open('README.md','r') as f:
     long_description = f.read()
 
-VERSION = '0.0.1'
-DESCRIPTION = 'create transformer models (articture behind chat)'
+VERSION = '0.0.8'
+DESCRIPTION = 'create transformer models (articture behind chat GPTand other large language models)'
 
 
 # Setting up
@@ -17,10 +17,10 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['re', 'pickle', 'numpy','keras','tensorflow'],
+    install_requires=['numpy','keras','tensorflow'],
     keywords=['python', 'ai', 'Chat GPT', 'transformer model', 'transformers', 'bert','seq2seq','sequence to sequence','classification','chat bot','deep learning','keras'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 5 - Production/Stable", 
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
