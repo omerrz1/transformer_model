bf529a13cfd3a97ce7d74d24a60e2f34aecb1a13
Omer Majdi <omermajdi@Omers-MacBook-Air.local>
2023-07-29 18:53:15 +0100

    added readme file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
-import codecs
-import os
 
+with open('README.md','r') as f:
+    long_description = f.read()
 
 VERSION = '0.0.1'
 DESCRIPTION = 'create transformer models (articture behind chat)'
-LONG_DESCRIPTION = "An all-in-one Python library for Transformers, offering streamlined data processing, model creation, training, and model serialization. Simplify your natural language processing tasks, whether it's text classification, language generation, or machine translation, using state-of-the-art transformer models. Effortlessly harness the power of transformers with a concise and user-friendly interface, enabling you to achieve impressive results with minimal coding overhead."
+
 
 # Setting up
 setup(
@@ -15,7 +15,7 @@ setup(
     author_email="<omermustafacontact@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=['re', 'pickle', 'numpy','keras','tensorflow'],
     keywords=['python', 'ai', 'Chat GPT', 'transformer model', 'transformers', 'bert','seq2seq','sequence to sequence','classification','chat bot','deep learning','keras'],
@@ -26,5 +26,6 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+    ],
+    python_requires=">=3.9"
 )
